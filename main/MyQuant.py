5a9e9e1201e24b6796b7385536d7c21e90296814
lkwoo <dlrmsdn135@naver.com>
2022-05-31 21:42:26 +0900

    feature : get_yield

--- main/MyQuant.py
+++ main/MyQuant.py
@@ -1,5 +1,6 @@
 import FinanceDataReader as fdr
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 # 종목코드, days일 이동평균
 def get_moving_average(code, days):    
@@ -14,13 +15,20 @@ def get_moving_average(code, days):
     for i in range(-days, 0):
         sum += df.iloc[i, 0]
     
-    return (sum / days)
+    return int(sum / days)
     
 
-# 종목코드, 시작날짜, 끝날짜
-# code의 start부터 end까지의 수익률 리턴
-def get_yield(code, start, end):
-    pass
+# 종목코드, months개월 수익률
+def get_yield(code, months):
+    today = datetime.today()
+    end = today.strftime("%Y-%m-%d")
+    start = today - relativedelta(months=months)
+    start = start.strftime("%Y-%m-%d")
+    
+    df = fdr.DataReader(code, start, end)
+    df = df[['Close']]
+    
+    return (df.iloc[-1, 0] - df.iloc[0, 0]) / df.iloc[0, 0] * 100
 
 
 # 종목코드
@@ -30,6 +38,6 @@ def get_momentum_score(code) :
 
 
 if __name__ == "__main__":
-    print(get_moving_average('005930', 200)) # get 삼성전자 200일 이동평균
-    
+    # print(get_moving_average('005930', 200)) # get 삼성전자 200일 이동평균
+    print(get_yield('005930', 12)) # 삼성전자 1년 수익률
     
